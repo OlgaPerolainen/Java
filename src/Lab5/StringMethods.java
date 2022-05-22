1bfa38b028f2c7c8356995fef67b021ad5a192df
Olga P <perolaynen@yandex.ru>
2022-05-22 16:07:34 +0300

    Initial commit

--- src/Lab5/StringMethods.java
+++ src/Lab5/StringMethods.java
@@ -19,13 +19,11 @@ public class StringMethods {
 
     // Ex2
     public static boolean isPalindrome(String word) {
-        boolean flag = true;
-        for (int i = 0; i < word.length() / 2; i++) {
-            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
-                flag = false;
-                break;
+        boolean flag = false;
+        StringBuilder wordForReverse = new StringBuilder(word);
+        if (word.equals(wordForReverse.reverse().toString())) {
+                flag = true;
             }
-        }
         return flag;
     }
 
