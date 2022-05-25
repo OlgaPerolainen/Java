246b73af7a8c3826c6a179fbba4121037c030253
Olga P <perolaynen@yandex.ru>
2022-05-25 14:44:20 +0300

    Initial commit

--- src/Lab6/Ex4/Number.java
+++ src/Lab6/Ex4/Number.java
@@ -3,5 +3,5 @@ package Lab6.Ex4;
 import java.util.Scanner;
 
 public class Number {
-    int number = new Scanner(System.in).nextInt();
+    protected int number = new Scanner(System.in).nextInt();
 }
