af76beebc8674b38ab0cac48d1986a5174966d28
Olga P <perolaynen@yandex.ru>
2022-05-25 21:30:05 +0300

    Initial commit

---
+++ src/Lab7/Main.java
@@ -0,0 +1,9 @@
+package Lab7;
+
+public class Main {
+    public static void main(String[] args) {
+        Plane plane = new Plane();
+        System.out.println(plane.getWing());
+        System.out.println(plane.getWing2());
+    }
+}
