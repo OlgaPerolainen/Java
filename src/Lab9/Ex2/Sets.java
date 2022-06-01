2adcbaca669b716a0282b616f45c07f970ea6d77
Olga P <perolaynen@yandex.ru>
2022-06-01 18:30:55 +0300

    Initial commit

--- src/Lab9/Ex2/Sets.java
+++ src/Lab9/Ex2/Sets.java
@@ -2,17 +2,18 @@ package Lab9.Ex2;
 
 import java.util.Arrays;
 import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class Sets {
-    public static LinkedHashSet<String> minusDoubles(String[] sourceArray) {
+    public static Set<String> minusDoubles(String[] sourceArray) {
         return new LinkedHashSet<>(Arrays.asList(sourceArray));
     }
 
-    public static LinkedHashSet<Integer> minusDoubles(Integer[] sourceArray) {
+    public static Set<Integer> minusDoubles(Integer[] sourceArray) {
         return new LinkedHashSet<>(Arrays.asList(sourceArray));
     }
 
-    public static LinkedHashSet<Double> minusDoubles(Double[] sourceArray) {
+    public static Set<Double> minusDoubles(Double[] sourceArray) {
         return new LinkedHashSet<>(Arrays.asList(sourceArray));
     }
 }
